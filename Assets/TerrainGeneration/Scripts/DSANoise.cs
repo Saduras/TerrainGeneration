c8d3b37cf49cd90626cb1b4c3a440975409f8bb2
David Speck <david@saduras.de>
2015-01-13 13:23:05 +0100

    Initial commit: - diamond square algorithmic height map generation - demo with standard assets

---
+++ Assets/TerrainGeneration/Scripts/DSANoise.cs
@@ -0,0 +1,158 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class DSANoise {
+
+	public delegate float CalculateRoughness(float average, float max, float size);
+
+	CalculateRoughness m_roughness;
+	int[,] _map;
+	int _max;
+
+	public DSANoise(int detail) {
+		_max = (int) Mathf.Pow(2, detail);
+
+		_map = new int[_max + 1, _max + 1];
+
+		// set default control values
+		int half = (_max + 1) / 2;
+		SetControlValues(half, half, half, half);
+
+		m_roughness = (average, max, size) => {
+			return Mathf.Pow(2, -1.0f);
+		};
+	}
+
+	public void SetControlValues(int a, int b, int c, int d) {
+		_map[0,0] = a;
+		_map[_max,0] = b;
+		_map[0,_max] = c;
+		_map[_max,_max] = d;
+	}
+
+	public void SetRoughnessFunction(CalculateRoughness roughness) 
+	{
+		m_roughness = roughness;
+	}
+
+	public void Generate() {
+		Divide(_max);
+	}
+
+	public float[,] GetNoiseMap()
+	{
+		float[,] result = new float[_max + 1, _max + 1];
+
+		for (int i = 0; i < _map.GetLength(0); i++) {
+			for (int j = 0; j < _map.GetLength(1); j++) {
+				result[i,j] = _map[i,j] / (float)_max;
+			}
+		}
+
+		return result;
+	}
+	
+	void Divide(int size) {
+		int x, y;
+		int half = size / 2;
+		float average;
+		int scale = size;
+
+		// recursion cancel condition
+		if (half < 1) return;
+
+		// iterate over square centers for current size
+		for (x = half; x < _max; x += size)
+		{
+			for (y = half; y < _max; y += size)
+			{
+				average = SquareAverage(x, y, half);
+				float roundness = GetRoundness(average, size);
+				int random = Mathf.FloorToInt(Random.Range(-scale * roundness, scale * roundness));
+				_map[x,y] = Mathf.FloorToInt(Mathf.Clamp(average + random, 0, _max));
+			}
+		}
+
+		// iterate over diamond center for current size
+		for (x = 0; x <= _max; x += half)
+		{
+			for (y = (x + half) % size; y <= _max; y += size)
+			{
+				average = DiamondAverage(x, y, half);
+				float roundness = GetRoundness(average, size);
+				int random = Mathf.FloorToInt(Random.Range(-scale * roundness, scale * roundness));
+				_map[x,y] = Mathf.FloorToInt(Mathf.Clamp(average + random, 0, _max));
+			}
+		}
+
+		Divide(half);
+	}
+
+	
+
+	/**
+	 * Calculate the average of vertices of a diamond 
+	 * with center in (x,y) and size (size)
+	 */
+	float SquareAverage(int x, int y, int size) {
+		// square vertices in clockwise order
+		int[][] vertices = new int[][]{
+			new int[]{x - size, y - size},
+			new int[]{x + size, y - size},
+			new int[]{x + size, y + size},
+			new int[]{x - size, y + size}
+		};
+
+		// check if vertex is on the map
+		List<float> values = new List<float>();
+		for (int i = 0; i != 4; i++)
+		{
+			if (IsInRange(vertices[i][0], 0, _max)
+				&& IsInRange(vertices[i][1], 0, _max)) {
+				values.Add(_map[vertices[i][0], vertices[i][1]]);
+			}
+		}
+		
+		return values.Average();
+	}
+
+	/**
+	* Calculate the average of vertices of a square
+	* with center in (x,y) and size (size)
+	*/
+	float DiamondAverage(int x, int y, int size) {
+		// diamond vertices in clockwise order
+		int[][] vertices = new int[][]{
+			new int[]{x, y - size},
+			new int[]{x + size, y},
+			new int[]{x, y + size},
+			new int[]{x - size, y}
+		};
+
+		// check if vertex is on the map
+		List<float> values = new List<float>();
+		for (int i = 0; i != 4; i++)
+		{
+			if (IsInRange(vertices[i][0], 0, _max)
+				&& IsInRange(vertices[i][1], 0, _max)) {
+				values.Add(_map[vertices[i][0],vertices[i][1]]);
+			}
+		}
+
+		return values.Average();
+	}
+
+	float GetRoundness(float average, float size)
+	{
+		return Mathf.Clamp(m_roughness(average, _max, size), 0, 1);
+	}
+
+	/**
+	 * Check whether the value is in the closed interval [min, max] or not.
+	 */
+	bool IsInRange(int value, int min, int max) {
+		return (value >= min) && (value <= max);
+	}
+}
---
+++ Assets/TerrainGeneration/Scripts/Editor/TerrainGeneratorWindow.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class TerrainGeneratorWindow : EditorWindow
+{
+	Terrain m_terrain = null;
+	AnimationCurve m_roundnessCurve = new AnimationCurve();
+	int m_detail = 8;
+
+	float m_smoothCenter = 0.5f;
+	float m_smoothGrade = 0.5f;
+	float m_smoothThreshold = 0.1f;
+	int m_smoothIterations = 2;
+
+	[MenuItem("Window/TerrainGenerator")]
+	static void Init()
+	{
+		GetWindow<TerrainGeneratorWindow>();
+	}
+
+	void OnGUI()
+	{
+		m_terrain = (Terrain)EditorGUILayout.ObjectField("Terrain",m_terrain, typeof(Terrain));
+
+		if (m_terrain != null) {
+			m_roundnessCurve = EditorGUILayout.CurveField("Roundness", m_roundnessCurve);
+			m_detail = EditorGUILayout.IntField("Detail", m_detail);
+
+			if (GUILayout.Button("Generate")) {
+				DSANoise noise = new DSANoise(m_detail);
+				noise.SetRoughnessFunction((average, max, size) => {
+					return Mathf.Pow(2, -0.5f);
+				});
+				noise.Generate();
+				m_terrain.terrainData.SetHeights(0, 0, noise.GetNoiseMap());
+			}
+
+			m_smoothCenter = EditorGUILayout.FloatField("Smooth center", m_smoothCenter);
+			m_smoothGrade = EditorGUILayout.FloatField("Smooth grade", m_smoothGrade);
+			m_smoothThreshold = EditorGUILayout.FloatField("Smooth threshold", m_smoothThreshold);
+			m_smoothIterations = EditorGUILayout.IntField("Smooth iterations", m_smoothIterations);
+
+			if (GUILayout.Button("Smooth")) {
+				float[,] heightMap = m_terrain.terrainData.GetHeights(0, 0, m_terrain.terrainData.heightmapWidth, m_terrain.terrainData.heightmapHeight);
+				for (int i = 0; i < m_smoothIterations; i++) {
+					heightMap = SmoothHeightMap(heightMap);
+				}
+				m_terrain.terrainData.SetHeights(0,0,heightMap);
+			}
+		}
+	}
+
+	float[,] SmoothHeightMap(float[,] heightMap) {
+
+		for (int i = 0; i < heightMap.GetLength(0); i++) {
+			for (int j = 0; j < heightMap.GetLength(1); j++) {
+				float diffToCenter = m_smoothCenter - heightMap[i, j];
+				if ( Mathf.Abs(diffToCenter) < m_smoothThreshold) {
+					heightMap[i, j] = heightMap[i, j] + Mathf.Sign(diffToCenter) * (m_smoothThreshold - Mathf.Abs(diffToCenter)) * m_smoothGrade;
+				}
+			}
+		}
+
+		return heightMap;
+	}
+}
