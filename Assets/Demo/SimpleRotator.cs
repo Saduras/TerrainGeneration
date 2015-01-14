9324c41ff1a4128ef0649bb96324d05a0cabcddd
David Speck <david@saduras.de>
2015-01-14 16:15:03 +0100

    Add simple rotation to camera

---
+++ Assets/Demo/SimpleRotator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleRotator : MonoBehaviour {
+
+	public float Speed = 1f;
+
+	float m_currentAngle;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		m_currentAngle += Speed * 360f * Time.deltaTime;
+		transform.localRotation = Quaternion.AngleAxis(m_currentAngle, Vector3.up);
+	}
+}
