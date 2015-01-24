200a92358bb15e796ec7b74eb721d1f1cc17c372
David Speck <david@saduras.de>
2015-01-24 12:10:02 +0100

    Add custom inspector for TerrainGenerator and TerrainTexturer to make them easy usable in the Editor

---
+++ Assets/TerrainGeneration/Scripts/Editor/TerrainGeneratorEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(TerrainGenerator))]
+public class TerrainGeneratorEditor : Editor 
+{
+	public override void OnInspectorGUI()
+	{
+		TerrainGenerator generator = (TerrainGenerator)target;
+
+		DrawDefaultInspector();
+		if (GUILayout.Button("Generate")) {
+			generator.Generate();
+		}
+
+	}
+}
---
+++ Assets/TerrainGeneration/Scripts/Editor/TerrainTexturerEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(TerrainTexturer))]
+public class TerrainTexturerEditor : Editor 
+{
+	public override void OnInspectorGUI()
+	{
+		TerrainTexturer texturer = (TerrainTexturer)target;
+
+		DrawDefaultInspector();
+		if (GUILayout.Button("Generate")) {
+			texturer.TextureTerrain();
+		}
+
+	}
+}
